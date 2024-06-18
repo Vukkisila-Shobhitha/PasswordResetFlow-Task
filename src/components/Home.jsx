f6ea98126e6e6acdd0ee6259c46bd7a2d3f23c59
Vukkisila-Shobhitha <shobitha6294@gmail.com>
2024-06-18 17:50:31 +0530

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import homeBanner from '../assets/kv3.jpg'
+
+const Home = () => {
+  return (
+    <>
+      <img src={homeBanner} className='w-100' style={{ height: '89.9vh' }} alt='Home Appliances' />
+    </>
+  )
+}
+
+export default Home
